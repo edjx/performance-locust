a8590b99030760a2a8f879603268591e486f9dfb
Ishan-edjx <ishan@edjx.io>
2021-10-14 17:27:36 +0530

    add delete applications scripts

--- functions.py
+++ functions.py
@@ -12,12 +12,18 @@ def id_generator(size=6):
 
 
 class Function(SequentialTaskSet):
+
+    # Constant(wait_time), Constant_pacing(wait_time), between(min, max),
+    # These function inject time b/w tasks
+    wait_time = constant(1)
+
     # token_string = ""
     def __init__(self, parent):
         print("init function \n")
         super().__init__(parent)
         self.token_string = ""
         self.org_id = ""
+        self.app_id = ""
         self.headers = {}
 
     def on_start(self):
@@ -34,8 +40,8 @@ class Function(SequentialTaskSet):
     def login_user(self):
         response = self.client.post("/guest/login", json=
         {
-            "email": "testedjx@mailinator.com",
-            "password": "Hello@123!"
+            "email": "edjperformance@mailinator.com",
+            "password": "Hello@123"
         }
                                     )
         # fetch token and org
@@ -52,32 +58,34 @@ class Function(SequentialTaskSet):
             headers=self.headers
         )
         print("Task 1")
-        # print("\n token: " + self.user.token_string)
         # print("\n response: " + response.text)
-        json_var = response.json()
+        # json_var = response.json()
         # print("\n applications: " + str(json_var))
-
-        # print(self.user.org_id)
 
     @task
     def create_applications(self):
         random_name = id_generator(3)
         payload = json.dumps({
             "name": random_name,
-            "organization": "d455f391-17c0-4057-9661-51294010f41c",
+            "organization": "" + self.org_id + "",
             "status": "active"
         })
-        response1 = self.client.post("/api/applications", headers=self.headers, data=payload)
         print("Task 2")
-        # print(self.headers)
-        print(payload)
-        print(self.org_id)
-        print("\n response: " + response1.text)
-        # print("\n applications: " + str(json_var))
+        response = self.client.post("/api/applications", headers=self.headers, data=payload)
+        json_var = response.json()
+        self.app_id = json_var["id"]
+        print("\n appID: " + self.app_id)
+
+    @task
+    def delete_applications(self):
+        print("Task 3")
+        response = self.client.delete("/api/applications/" + self.app_id, headers=self.headers)
+        json_var = response.json()
+        print("\n delete response: " + str(json_var))
 
 
 # Define user/ httpuser to trigger and execute functionperf class
 class TaskExecutor(HttpUser):
     host = "https://api.load.edjx.network"
     tasks = [Function]
-    wait_time = constant(1)
+
