b5f79652e88af5b8cb20e020f468bd3a6e790bab
Ishan-edjx <ishan@edjx.io>
2021-10-20 01:35:44 +0530

    Create and deploy functions

--- functions.py
+++ functions.py
@@ -12,7 +12,6 @@ def id_generator(size=6):
 
 
 class Function(SequentialTaskSet):
-
     # Constant(wait_time), Constant_pacing(wait_time), between(min, max),
     # These function inject time b/w tasks
     wait_time = constant(1)
@@ -24,6 +23,7 @@ class Function(SequentialTaskSet):
         self.token_string = ""
         self.org_id = ""
         self.app_id = ""
+        self.func_name = ""
         self.headers = {}
 
     def on_start(self):
@@ -57,7 +57,7 @@ class Function(SequentialTaskSet):
             "/api/applications?limit=1",
             headers=self.headers
         )
-        print("Task 1")
+        print("Read application")
         # print("\n response: " + response.text)
         # json_var = response.json()
         # print("\n applications: " + str(json_var))
@@ -70,15 +70,67 @@ class Function(SequentialTaskSet):
             "organization": "" + self.org_id + "",
             "status": "active"
         })
-        print("Task 2")
+        print("Create Application")
         response = self.client.post("/api/applications", headers=self.headers, data=payload)
         json_var = response.json()
         self.app_id = json_var["id"]
         print("\n appID: " + self.app_id)
 
     @task
+    def create_deploy_functions(self):
+        random_name = id_generator(4)
+
+        # Create
+        payload = json.dumps({
+            "name": random_name,
+            "properties": {
+                "language": "rust",
+                "memoryAllocated": 134217728,
+                "runtime": "wasm",
+                "timeout": 30000,
+                "trigger": "http"
+            },
+            "policies": []
+        })
+        print("Create function")
+        response = self.client.post("/api/applications/" + self.app_id + "/deployments",
+                                    headers=self.headers,
+                                    data=payload)
+        json_var = response.json()
+        # print("Create func" + response.text)
+        self.func_name = json_var["name"]
+
+        deploy_section = json_var["deploy"]
+        update_section = json_var["update"]
+        deploy_url = deploy_section["href"]
+        callback_url = update_section["href"]
+
+        print("\n func_name: " + self.func_name)
+        print("\n deploy url: " + deploy_url)
+        print("\n callback url: " + callback_url)
+
+        # deploy
+        print("Deployment")
+        filename = 'hello_world.wasm'
+        payload = {'peer-addrs': '',
+                   'notary': 'false',
+                   'callBackURL': callback_url
+                   }
+        files = [
+            ('content', (filename, open(filename, 'rb'),
+                         'application/octet-stream'))
+        ]
+        headers = {
+            'Authorization': 'Bearer ' + self.token_string
+        }
+
+        response = self.client.put(deploy_url, headers=headers, data=payload, files=files)
+        # json_var = response.json()
+        print("Deployment response -> : " + response.text)
+
+   # @task
     def delete_applications(self):
-        print("Task 3")
+        print("delete applications")
         response = self.client.delete("/api/applications/" + self.app_id, headers=self.headers)
         json_var = response.json()
         print("\n delete response: " + str(json_var))
@@ -88,4 +140,3 @@ class Function(SequentialTaskSet):
 class TaskExecutor(HttpUser):
     host = "https://api.load.edjx.network"
     tasks = [Function]
-
